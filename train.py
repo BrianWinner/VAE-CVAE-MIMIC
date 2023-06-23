6a99d0e809508aeab37dbf2917b7304764543d0c
BrianWinner <70809839+BrianWinner@users.noreply.github.com>
2023-06-23 11:21:36 -0500

    Backup from June 12th, 2023

--- train.py
+++ train.py
@@ -19,17 +19,7 @@ from ray.tune.schedulers import ASHAScheduler
 
 from models import VAE
 
-
-def main(args):
-
-    torch.manual_seed(args.seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed(args.seed)
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    ts = time.time()
-
+def load_data(config):
     # dataset = IHMDataset(
     #     root='/data/datasets/mimic3-benchmarks/data/root', train=True)
     
@@ -39,19 +29,31 @@ def main(args):
     print("Finished dataset initializing")
     
     data_loader = DataLoader(
-        dataset=dataset, batch_size=args.batch_size, shuffle=True)
+        dataset=dataset, batch_size=config["batchSize"], shuffle=True)
 
     print("finished dataloader initializing")
     
-    def loss_fn(recon_x, x, mean, log_var):
-        loss = torch.nn.MSELoss()
-        lossActual = loss(x, recon_x)
+    return data_loader
 
-        KLD = (-0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp()) ) / x.size(0)
+def loss_fn(recon_x, x, mean, log_var):
+    loss = torch.nn.MSELoss()
+    lossActual = loss(x, recon_x)
 
-        return (lossActual + KLD)
-        
+    KLD = (-0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp()) ) / x.size(0)
 
+    return (lossActual + KLD)
+
+def train(config):
+    torch.manual_seed(args.seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(args.seed)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    ts = time.time()
+    
+    data_loader = load_data(config)
+    
     vae = VAE(
         encoder_layer_sizes=args.encoder_layer_sizes,
         latent_size=args.latent_size,
@@ -59,17 +61,15 @@ def main(args):
         conditional=args.conditional,
         num_labels=10 if args.conditional else 0).to(device)
 
-    optimizer = torch.optim.Adam(vae.parameters(), lr=args.learning_rate)
+    optimizer = torch.optim.Adam(vae.parameters(), lr=config["lr"])
 
     logs = defaultdict(list)
     
-    print("Epochs: {:02d} Batch Size: {:02d} Learning Rate: {:.4f}".format(args.epochs, args.batch_size, args.learning_rate))
+    print("Epochs: {:02d} Batch Size: {:02d} Learning Rate: {:.4f}".format(config["epochs"], config["batchSize"], config["lr"]))
     
     total_loss = 0.0
     
-    for epoch in range(args.epochs):
-
-        tracker_epoch = defaultdict(lambda: defaultdict(dict))
+    for epoch in range(config["epochs"]):
 
         for iteration, (x, y, sl, m) in enumerate(data_loader):
             
@@ -87,15 +87,10 @@ def main(args):
             # print(recon_x.shape)
             # print(x.shape)
             
-            #tracker_epoch only used in graphs part
-            # for i, yi in enumerate(y):
-            #     id = len(tracker_epoch)
-            #     tracker_epoch[id]['x'] = z[i, 0].item()
-            #     tracker_epoch[id]['y'] = z[i, 1].item()
-            #     tracker_epoch[id]['label'] = yi.item()
-
             #CALCULATE LOSS
             loss = loss_fn(recon_x, x, mean, log_var)
+            
+            total_loss += loss.item()
 
             #BACK PROP
             optimizer.zero_grad()
@@ -105,54 +100,32 @@ def main(args):
             #ADDING LOSS TO LOGS
             logs['loss'].append(loss.item())
 
-            #BELOW IS ALL PRINTING AND GRAPH MAKING
-            # if iteration % args.print_every == 0 or iteration == len(data_loader)-1:
-            #     print("Epoch {:02d}/{:02d} Batch {:04d}/{:d}, Loss {:9.4f}".format(
-            #         epoch, args.epochs, iteration, len(data_loader)-1, loss.item()))
-            
+            #BELOW IS ALL PRINTING AND GRAPH MAKING     
             if iteration == len(data_loader)-1:
-                print("Epoch {:02d}/{:02d} Batch {:04d}/{:d}, Loss {:9.4f}".format(
-                    epoch, args.epochs, iteration, len(data_loader)-1, loss.item()))
-
-                # if args.conditional:
-                #     c = torch.arange(0, 10).long().unsqueeze(1).to(device)
-                #     z = torch.randn([c.size(0), args.latent_size]).to(device)
-                #     x = vae.inference(z, c=c)
-                # else:
-                #     z = torch.randn([10, args.latent_size]).to(device)
-                #     x = vae.inference(z)
-
-#                 plt.figure()
-#                 plt.figure(figsize=(5, 10))
-#                 for p in range(10):
-#                     plt.subplot(5, 2, p+1)
-#                     if args.conditional:
-#                         plt.text(
-#                             0, 0, "c={:d}".format(c[p].item()), color='black',
-#                             backgroundcolor='white', fontsize=8)
-#                     plt.imshow(x[p].view(28, 28).cpu().data.numpy())
-#                     plt.axis('off')
-
-#                 if not os.path.exists(os.path.join(args.fig_root, str(ts))):
-#                     if not(os.path.exists(os.path.join(args.fig_root))):
-#                         os.mkdir(os.path.join(args.fig_root))
-#                     os.mkdir(os.path.join(args.fig_root, str(ts)))
-
-#                 plt.savefig(
-#                     os.path.join(args.fig_root, str(ts),
-#                                  "E{:d}I{:d}.png".format(epoch, iteration)),
-#                     dpi=300)
-#                 plt.clf()
-#                 plt.close('all')
-
-        # df = pd.DataFrame.from_dict(tracker_epoch, orient='index')
-        # g = sns.lmplot(
-        #     x='x', y='y', hue='label', data=df.groupby('label').head(100),
-        #     fit_reg=False, legend=True)
-        # g.savefig(os.path.join(
-        #     args.fig_root, str(ts), "E{:d}-Dist.png".format(epoch)),
-        #     dpi=300)
-
+                print("Epoch {:02d} Batch {:04d}/{:d}, Loss {:9.4f}".format(
+                    epoch, iteration, len(data_loader)-1, loss.item()))
+                
+        session.report(
+            {"loss": loss.item()},
+        )
+        
+        print("Training complete")
+    
+def main(args):
+    config = {
+        "lr": tune.loguniform(1e-4, 1e-0),
+        "batchSize": tune.choice([16, 32, 64, 128, 256]),
+        "epochs": tune.choice([5, 10, 15, 20, 25, 30, 35, 40]),
+    }
+    
+    result = tune.run(
+        train,
+        config=config,
+    )
+    
+    best_trial = result.get_best_trial("loss", "min", "last")
+    print(f"Best config: {best_trial.config}")
+    print(f"Best loss: {best_trial.last_result['loss']}")
 
 if __name__ == '__main__':
     
